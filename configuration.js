6a46994a436b9f17de5f21a14bb288a80eed1e60
Elena <e.gubarenko@belatra.com>
2022-07-13 14:56:25 +0300

    add ch

--- configuration.js
+++ configuration.js
@@ -1,6 +1,8 @@
 export default {
     rows: 2,
     cols: 5,
-    cardsId: [1, 2, 3, 4, 5]
+    cardsId: [1, 2, 3, 4, 5],
+    cardsFrontsNames: ['henata-card.png', 'kakashi-card.png', 'naruto-card.png', 'sakura-card.png', 'saske-card.png'],
+    cardsBack: ['cardBack.png']
 }
 
